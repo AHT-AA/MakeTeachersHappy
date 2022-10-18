e3f61c0f28f11632175e3fadadf09c15f1e2a64a
AHT-AA <112618774+AHT-AA@users.noreply.github.com>
2022-10-18 19:34:12 +0300

    Add files via upload

--- COMPILE.cmd
+++ COMPILE.cmd
@@ -4,9 +4,9 @@ rem set path=C:\mingw64\bin
 
 
 
-@del MakeTechersHappy.exe > NUL
+@del MakeTeachersHappy.exe > NUL
 
 
-gcc main.c -o MakeTechersHappy.exe -Wall -Wextra -mwindows -m32 -std=c11 -s -I .
+gcc main.c -o MakeTeachersHappy.exe -Wall -Wextra -mwindows -m32 -std=c11 -s -I .
 
 @PAUSE
